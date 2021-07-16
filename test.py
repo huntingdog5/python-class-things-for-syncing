b03e8fc48ef73900b688058fa99390340c0d5917
huntingdog5 <80111060+huntingdog5@users.noreply.github.com>
2021-07-16 19:54:25 -0400

    progress

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
 
 #for usages testing arepl things
-x = { 'chuck' : 1 , 'fred' : 42, 'jan': 100}
-y = x.items()
+import re
+def fileset():
+    prmpt = input('enter file name/path:\n')
+    #prmpt = input('enter file name/path\n')
+    # prmpt = 'mbox-short.txt'
+    if prmpt == '':
+        filepath = 'mbox-short.txt'
+    else:
+        try:
+            filepath = prmpt
+        except:
+            print('file cant be opened defaulting to sample file')
+            filepath = 'mbox-short.txt'
+    return filepath
+def re_doer():
+    file = open(fileset())
+    re_set = input('enter reg expression')
+    for line in file:
+        line = line.rstrip()
+        x = re.findall(re_set, line):
+        if len(x) > 0
 
-print(type(y))
+re_doer()
